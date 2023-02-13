df1ae7cbabe54d1c5c58be2c637bcad6bd38b6f5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-13 20:14:27 +0100

    feat: :sparkles: import modules, add some buttons for testing, add comments to try to explain stuff

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
+// se ChatGPT förklarar ECMAscriptmoduler.md
+import { filterByName, getAllLists } from "./module-api.js";
+
+// inte använd men länken till början av APIt
 const API_BASE = "https://nackademin-item-tracker.herokuapp.com/";
 
-let body = document.querySelector("body");
-let p = document.createElement("p");
-let button = document.createElement("button");
-button.innerText = "click";
-button.addEventListener("click", async () => getAllLists());
+// Globala variabler (for now)
+let name = "Henrik";
 
-async function getAllLists() {
-  const res = await fetch(
-    `https://nackademin-item-tracker.herokuapp.com/lists`
-  );
-  const data = await res.json();
-  // console.log(data);
-  p.innerHTML += data;
-  return data;
-}
-// let allLists = await getAllLists();
-body.append(button);
-body.append(p);
+// Queryselectors
+let body = document.querySelector("body");
+
+// Elements (bara för testing!)
+let getAllListsButton = document.createElement("button");
+getAllListsButton.innerText = "Get all lists";
+getAllListsButton.addEventListener("click", async () => getAllLists());
+body.append(getAllListsButton);
+
+let filterButton = document.createElement("button");
+filterButton.innerText = "Filter by name";
+filterButton.addEventListener("click", async () => filterByName(name));
+body.append(filterButton);
+
+// exempel på att bara kalla funktionen som skriver ut i DOM
+console.log(
+  "här är ID för en listig lista: " + (await filterByName("ListigLista"))
+);
+
+// spara listorna i en variabel lists (ta bort stringify om man bara vill ha objekten)
+let lists;
+
+lists = await getAllLists();
+lists = JSON.stringify(lists);
+
+console.log("All lists:\n" + lists);

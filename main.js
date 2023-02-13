d7f41aa66709e22a1eeeecde3a6d96739c7ef5f1
Viktoria Lindberg <viktorial86@hotmail.com>
2023-02-13 14:47:06 +0100

    button-work

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const createListBtn = document.getElementsByClassName('newListBtn');
-
+const createListBtn = document.getElementById('newListBtn');
+const currentContentDiv = document.getElementById('current-content');
 
 createListBtn.addEventListener("click", ()=>{
-    
+
+    console.log("hej");
 
 })
 

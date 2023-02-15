793a61289fe6df9433a4a50e9b9c8a3b75690a12
Viktoria Lindberg <viktorial86@hotmail.com>
2023-02-15 13:15:42 +0100

    liten commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { createDebugElements } from "./module-debug.js";
 const API_BASE = "https://nackademin-item-tracker.herokuapp.com/";
 
 // Globala variabler (for now)
-let debugMode = false;
+let debugMode = true;
 
 // skapar debugelement om debugMode är true
 createDebugElements(debugMode);
@@ -17,6 +17,15 @@ const headerName = document.querySelector(".headerNameEdit");
 const listItemsUl = document.querySelector("#listItems");
 const saveBtnDiv = document.querySelector(".saveToApi");
 
+let currentList = '';
+let itemListArray = [];
+
+let listObject = {
+  customField: "grupp_e",
+  itemList: itemListArray, 
+  listname: "another list"
+}
+
 //Plus-knappen på index-sidan som ska ta en till skapa lista-vyn.
 createListBtn.addEventListener("click", (event) => {
   createListBtn.style = `display: none`;
@@ -27,11 +36,13 @@ createListBtn.addEventListener("click", (event) => {
     <input type="text" class="nameinput" value="New List"></input>
     <button><img src="assets/three-dots-vertical.svg" alt=""></button>
     `;
+    
     //Eventlistener för "gå tillbaka-knappen"
   const backBtn = document.querySelector(".backBtn");
   backBtn.addEventListener("click", () => {
        window.location.href = "index.html";
   });
+
 
   //Element för inputfält där man skriver list-item + lägg till-knapp
   addAnItemDiv.innerHTML = `
@@ -46,15 +57,15 @@ createListBtn.addEventListener("click", (event) => {
     
     if(listItemInput.value !== null && listItemInput.value !== ''){
     let li = document.createElement("li");
-    li.innerHTML = `<span class="iconspans"><img src="assets/trash.svg" alt=""></span><input type="text" value="${listItemInput.value}"></input>
+    li.innerHTML = `<span class="iconspans"><img src="assets/trash.svg" alt="" width="12px"></span><input type="text" value="${listItemInput.value}"></input>
             `;
     listItemsUl.append(li);
+    itemListArray.push({title:listItemInput.value, checked: false});
+    console.log(itemListArray);
     listItemInput.value = "";
     } else {
         alert("Fyll i ett item för att lägga till")
     }
-
-    //FRÅGA:: Ska man skicka in värdet på list-itemet här i en lokal lista/objekt?
   });
 
   //Spara-knapp som ska ha eventlistener/funktion att skicka datan till api:et
@@ -62,10 +73,57 @@ createListBtn.addEventListener("click", (event) => {
   saveToAPIBtn.className = "saveBtn";
   saveToAPIBtn.innerText = "Save List";
   saveBtnDiv.append(saveToAPIBtn);
+
+  saveToAPIBtn.addEventListener("click", ()=>{
+    saveList();
+    itemListArray.forEach((object)=>{
+      saveItems();
+    })
+    
+  })
 });
 
 //Man lär behöva ha en loop då som loopar igenom alla Li-items och tar deras value och skickar in via POST?
 
+async function saveList(){
+  const listNameInput = document.querySelector(".nameinput");
+  const listname = listNameInput.value;
+const customfield = "grupp_e";
+const res = await fetch(`https://nackademin-item-tracker.herokuapp.com/lists`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    listname: listname,
+    customfield: "grupp_e",
+  }),
+});
+const { list } = await res.json();
+console.log(list)
+currentList = list._id;
+console.log(currentList);
+}
 
+async function saveItems(){
+  
+const title = itemListArray.title;
+const checked = itemListArray.checked;
+const res = await fetch(
+  `https://nackademin-item-tracker.herokuapp.com/lists/${currentList}/items`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      checked: checked,
+    }),
+  }
+);
+const { list } = await res.json();
+console.log(list);
+}
 
 

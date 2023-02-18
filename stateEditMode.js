990af6e5bbf847dfcc14b289c15d7c0d4af0e660
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-18 16:50:03 +0100

    fix: :recycle: move itemlist edit function here
    if currentList is not "" it is an existing list and so use update logic (TODO), if not, use the create list logic

--- stateEditMode.js
+++ stateEditMode.js
@@ -1,50 +1,104 @@
 export function editMode({ selectedList, listItemsUl, API_BASE, headerName }) {
+  const outputElement = document.querySelector("#current-content");
+  let currentList = "";
+  let itemListArray = [];
+
+  let listObject = {
+    customField: "grupp_e",
+    itemList: itemListArray,
+    listname: "another list",
+  };
+
+  //Element för inputfält där man skriver list-item + lägg till-knapp
+  let addAnItemDiv = document.createElement("div");
+  addAnItemDiv.innerHTML = `
+     <input type="text" class="listiteminput" placeholder="Add an item..."></input>
+     <button class="additembtn">+</button>
+     `;
+  outputElement.append(addAnItemDiv);
+
+  const listItemInput = document.querySelector(".listiteminput");
+  const addItemBtn = document.querySelector(".additembtn");
+
+  listItemInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      if (containsSpecialChars(listItemInput.value)) {
+        alert("No special characters are allowed");
+      } else {
+        addItem();
+      }
+    }
+  });
+
   let bottomButton = document.querySelector("#newListBtn");
   bottomButton.classList.add("hidden");
-  console.log(selectedList._id);
+  if (selectedList) {
+    selectedList.itemList.forEach((item) => {
+      // console.log("ITEM IS" + item.title)
+      itemListArray.push(item);
+    });
+    console.log("editing list");
 
-  let listNamn = selectedList.listname;
-  selectedList.itemList.forEach((item) => {
-    let listItem = document.createElement("li");
+    let listNamn = selectedList.listname;
 
-    /* const checkboxInput = document.createElement("input"); */
-    /* checkboxInput.type = "checkbox"; //typen av input
+    function deleteObject(title, element) {
+      let index = itemListArray.findIndex((object) => object.title === title);
+      itemListArray.splice(index, 1);
+      element.remove();
+    }
+
+    selectedList.itemList.forEach((item) => {
+      console.log(listNamn);
+      let listItem = document.createElement("li");
+
+      /* const checkboxInput = document.createElement("input"); */
+      /* checkboxInput.type = "checkbox"; //typen av input
         checkboxInput.name = item.title; //checkbox namn är list items namn
         checkboxInput.id = item._id; //id för checkbox är items id
         checkboxInput.value = item.checked; // chckbox value
         checkboxInput.checked = item.checked; //visas som checked/unchecked beroende på true false value från item
   
         console.log(checkboxInput)  */
-    /* checked=${item.checked} */
+      /* checked=${item.checked} */
 
-    const labelA = document.createElement("label");
-    labelA.setAttribute("for", item._id);
-    labelA.classList.add("itemContainer");
-    labelA.innerHTML =
-      `<input type="checkbox" name=${item.title} id=${item._id} value=${item.checked} /><span class="checkmark"></span>` +
-      item.title;
+      const labelA = document.createElement("label");
+      labelA.setAttribute("for", item._id);
+      labelA.classList.add("itemContainer", "editing");
+      labelA.innerHTML = `<span class="iconspans"><img src="assets/trash.svg" id="${item.title.replaceAll(" ", "-")}" width="12px"></span>
+    <input type="text" value="${item.title}" id="item_${listItemInput.value}"></input>`;
 
-    console.log(labelA);
+      console.log(labelA);
 
-    item.checked
-      ? listItem.classList.add("checkedItem")
-      : listItem.classList.remove("checkedItem");
+      item.checked
+        ? listItem.classList.add("checkedItem")
+        : listItem.classList.remove("checkedItem");
 
-    /* listItem.appendChild(checkboxInput); */
-    listItem.appendChild(labelA);
+      /* listItem.appendChild(checkboxInput); */
+      listItem.appendChild(labelA);
 
-    /* labelA.innerHTML += item.qty ? ` ${item.qty}` : " :1"; */
+      /* labelA.innerHTML += item.qty ? ` ${item.qty}` : " :1"; */
 
-    // Funktion som ändrar om itemet är checked eller inte
+      // Funktion som ändrar om itemet är checked eller inte
 
-    listItemsUl.append(listItem);
-  });
-  headerName.innerHTML = "this is edit mode";
-  headerName.innerHTML += `
-      <span class="backBtn"><img src="assets/back-arrow.svg" alt=""></span>
-      <input type="text" class="nameinput" value="${listNamn}" onfocus="this.placeholder=''"></input>
-      <button id="button-editmode"><img class="hover" src="assets/three-dots-vertical.svg" alt=""></button>
-      `;
+      listItemsUl.append(listItem);
+
+      let removeBtn = document.querySelector(`#${item.title.replaceAll(" ", "-")}`);
+      console.log(removeBtn);
+      removeBtn.addEventListener("click", (event) => {
+        deleteObject(removeBtn.id, labelA);
+        console.log(itemListArray);
+      });
+    });
+    headerName.innerHTML = "this is edit mode";
+    headerName.innerHTML += `
+        <span class="backBtn"><img src="assets/back-arrow.svg" alt=""></span>
+        <input type="text" class="nameinput" value="${listNamn}" onfocus="this.placeholder=''"></input>
+        <button id="button-editmode"><img class="hover" src="assets/three-dots-vertical.svg" alt=""></button>
+        `;
+    console.log(selectedList._id);
+  } else {
+    console.log("creating list");
+  }
 
   //Eventlistener för "gå tillbaka-knappen"
   const backBtn = document.querySelector(".backBtn");
@@ -52,4 +106,162 @@ export function editMode({ selectedList, listItemsUl, API_BASE, headerName }) {
     bottomButton.classList.remove("hidden");
     window.location.href = "index.html";
   });
+
+  outputElement.append(listItemsUl);
+
+  //plus-knappen lägger till ett item i den "lokala" listan som gör att man kan redigera den innan den sparas till api
+  // change-event körs när man trycker på knappen för inputfältet tappar fokus
+  addItemBtn.addEventListener("click", () => {
+    addItem();
+  });
+
+  function addItem() {
+    if (containsSpecialChars(listItemInput.value)) {
+      alert("No special characters are allowed");
+    } else {
+      if (listItemInput.value !== null && listItemInput.value !== "") {
+        //OM list-itemets namn redan finns i vår lokala array, alert eller skriv ut den.
+        if (
+          itemListArray.some((object) => object.title === listItemInput.value)
+        ) {
+          alert("That item already exists, write another one!");
+        } else {
+          let li = document.createElement("li");
+          li.innerHTML = `<span class="iconspans"><img src="assets/trash.svg" id="${listItemInput.value}" width="12px"></span>
+        <input type="text" value="${listItemInput.value}" id="item_${listItemInput.value}"></input>`;
+          listItemsUl.append(li);
+          let removeBtn = document.getElementById(`${listItemInput.value}`);
+          let inputfields = document.getElementById(
+            `item_${listItemInput.value}`
+          );
+          console.log(inputfields);
+
+          //Funktion för att hitta rätt object i arrayen och ta bort den samtidigt som den tar bort utskriften
+          function deleteObject(title) {
+            let index = itemListArray.findIndex(
+              (object) => object.title === title
+            );
+            itemListArray.splice(index, 1);
+            li.remove();
+          }
+
+          itemListArray.push({ title: listItemInput.value, checked: false });
+          console.log(itemListArray);
+
+          removeBtn.addEventListener("click", (event) => {
+            deleteObject(removeBtn.id);
+            console.log(itemListArray);
+          });
+
+          function updateListItemLocally(previoustitle) {
+            let index = itemListArray.findIndex(
+              (object) => object.title === previoustitle
+            );
+            itemListArray.splice(index, 1);
+            itemListArray.splice(index, 0, {
+              title: changeValue,
+              checked: false,
+            });
+          }
+
+          let changeValue = "";
+          li.addEventListener("change", (event) => {
+            if (containsSpecialChars(event.target.value)) {
+              alert("No special characters are allowed");
+            } else {
+              changeValue = event.target.value;
+            }
+
+            let previousValue = event.target.id;
+            let previousVal = previousValue.replace("item_", "");
+            updateListItemLocally(previousVal);
+          });
+
+          listItemInput.value = "";
+        }
+      } else {
+        alert("You need to write something to add an item ;)");
+      }
+    }
+  }
+
+  let saveBtnDiv = document.createElement("div");
+  outputElement.append(saveBtnDiv);
+  //Spara-knapp som ska ha eventlistener/funktion att skicka datan till api:et
+  let saveToAPIBtn = document.createElement("button");
+  if (selectedList) {
+    saveToAPIBtn.className = "saveBtn";
+    saveToAPIBtn.innerText = "Update List";
+    saveBtnDiv.append(saveToAPIBtn);
+  } else {
+    saveToAPIBtn.className = "saveBtn";
+    saveToAPIBtn.innerText = "Save List";
+    saveBtnDiv.append(saveToAPIBtn);
+  }
+  saveToAPIBtn.addEventListener("click", () => {
+    if (selectedList) {
+      console.log("TODO (update list)");
+    } else {
+      saveList();
+      listItemsUl.innerHTML = "";
+      let p = document.createElement("p");
+      p.innerText = "Your list have been saved!";
+      p.style.color = "green";
+      saveBtnDiv.append(p); //töm fälten och meddela att listan sparats
+    }
+  });
+
+  function containsSpecialChars(str) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+  }
+
+  //Funktion för att spara listan med angivet namn
+  async function saveList() {
+    const listNameInput = document.querySelector(".nameinput");
+    const listname = listNameInput.value;
+    const customfield = "grupp_e";
+    const res = await fetch(
+      `https://nackademin-item-tracker.herokuapp.com/lists`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          listname: listname,
+          customfield: "grupp_e",
+        }),
+      }
+    );
+    const { list } = await res.json();
+    console.log(list);
+    currentList = list._id;
+    console.log(currentList);
+
+    saveItems();
+  }
+
+  async function saveItems() {
+    itemListArray.forEach(async (object) => {
+      const title = object.title;
+      const checked = object.checked;
+      const res = await fetch(
+        `https://nackademin-item-tracker.herokuapp.com/lists/${currentList}/items`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: title,
+            checked: checked,
+          }),
+        }
+      );
+
+      const { list } = await res.json();
+      console.log(list);
+    });
+  }
 }

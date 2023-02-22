a001feffd5715153bb4a5aa82e3008030f95473b
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-02-22 14:59:24 +0100

    feat(DOM output): :sparkles: add some functionality to settings button

--- module-settings.js
+++ module-settings.js
@@ -11,9 +11,50 @@ function settingsButtonFunction(e) {
   // console.log(`settings button clicked from ${e.currentTarget.currentState}`);
   if (e.currentTarget.currentState === "index") {
     console.log(`settings button clicked from our beautiful index page`);
+    hideIndexHeader();
   } else if (e.currentTarget.currentState === "viewOneList") {
     console.log(`settings button clicked from our amazing view list view`);
+    hideListViewTitle();
   } else if (e.currentTarget.currentState === "editOneList") {
     console.log(`settings button clicked from our spectacular edit list view`);
+    hideListViewTitle();
+  }
+  clearCurrentContent();
+  hideCreateListButton();
+  addHeaderTitle();
+}
+
+function clearCurrentContent() {
+  const currentContent = document.querySelector("#current-content");
+  currentContent.innerHTML = "";
+}
+
+function hideCreateListButton() {
+  const createListBtn = document.getElementById("newListBtn");
+  createListBtn.style = `display: none`;
+}
+
+function hideIndexHeader() {
+  const oldHeader = document.getElementById("header-name-title");
+  if (oldHeader) {
+    oldHeader.style = `display: none`;
+  }
+}
+
+function hideListViewTitle() {
+  const listTitle = document.querySelector(".nameinput");
+  if (listTitle) {
+    listTitle.style = `display: none`;
+  }
+}
+
+function addHeaderTitle() {
+  let headerNameTitle = document.createElement("h1");
+  headerNameTitle.id = "settings-title";
+  headerNameTitle.innerHTML = "Settings";
+  const headerName = document.querySelector(".headerNameEdit");
+  let oldSettingsTitle = document.querySelector("#settings-title");
+  if (!oldSettingsTitle) {
+    headerName.append(headerNameTitle);
   }
 }

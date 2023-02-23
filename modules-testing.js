89c1c7c4a4c76222d485a1304f4fb4c02f340ef4
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-23 14:53:36 +0100

    feat: :zap: replace img with svg in modules-testing.js

--- modules-testing.js
+++ modules-testing.js
@@ -118,7 +118,7 @@ export async function displayListsAlt() {
       let trashcan = document.createElement("span");
       trashcan.classList.add("remove-container", "hidden", "hover");
       trashcan.innerHTML =
-        '<img class="remove hover" src="assets/trash.svg" alt="trash icon">';
+        '<svg class="remove hover" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
       // ---------- DELETE-FUNCTION ----------
       trashcan.addEventListener("click", (e) => {
         e.stopPropagation();
@@ -163,7 +163,7 @@ export async function displayListsAlt() {
 
     //Bygger listans namninput-fält med fältet för namnet i headern
     headerName.innerHTML = `
-    <span class="backBtn"><img src="assets/back-arrow.svg" alt=""></span>
+    <span class="backBtn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></span>
     <input type="text" class="nameinput" value="New List"></input>
     `;
 

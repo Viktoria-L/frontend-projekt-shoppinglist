6f5b3ffe8c5b8e65103db4281ed2d32f7b9898c5
karlfredriko <kf.holmsten@gmail.com>
2023-02-27 10:26:21 +0100

    fix: :bug: finding right id

--- modules-testing.js
+++ modules-testing.js
@@ -1,12 +1,12 @@
 import { editMode } from "./stateEditMode.js";
 import { deleteListUsingID } from "./module-api.js";
-import { showSelectedList } from "./module-show-selected-list.js"
+import { showSelectedList } from "./module-show-selected-list.js";
 
 export async function displayListsAlt() {
   const currentContentContainer = document.getElementById("current-content");
 
   let eGroupLists = await getOurLists();
-  const API_BASE = "https://nackademin-item-tracker.herokuapp.com/"
+  const API_BASE = "https://nackademin-item-tracker.herokuapp.com/";
 
   printLists(eGroupLists);
 
@@ -43,19 +43,23 @@ export async function displayListsAlt() {
     //------ ---- ---- ---- ---- ---- ---- ----
 
     previewContainer.innerHTML = ""; //clearing the container of content
-    
-    //REVERSING FETCHEDLISTS AND PRINTING REVERSED ARRAY 
+
+    //REVERSING FETCHEDLISTS AND PRINTING REVERSED ARRAY
     let reversedFetchedLists = [...fetchedLists].reverse();
 
     console.log("original", fetchedLists);
     console.log("reversed", reversedFetchedLists);
-    
+
     reversedFetchedLists.forEach((list) => {
       //For each list item in the fetched array
       const previewObject = document.createElement("div");
       previewContainer.append(previewObject);
       // ?? efter ett value är fancy sätt att säga "om undefined/null, ge värdet till höger om ?? istället för undefined/null"
-      previewObject.classList.add("preview-object", "hover", `list-color-${list.color ?? "default"}`);
+      previewObject.classList.add(
+        "preview-object",
+        "hover",
+        `list-color-${list.color ?? "default"}`
+      );
 
       previewObject.dataset.listId = list._id;
 
@@ -83,10 +87,10 @@ export async function displayListsAlt() {
       previewObject.innerHTML += `<h2>${list.listname} </h2> `;
       if (list.itemList && Array.isArray(list.itemList)) {
         // previewObject.innerHTML += `<ul>`;
-        
+
         let listUl = document.createElement("ul");
         previewObject.appendChild(listUl);
-        
+
         list.itemList.forEach((item) => {
           if (count >= 3) {
             if (!previewObject.innerHTML.includes("...")) {
@@ -94,20 +98,19 @@ export async function displayListsAlt() {
             }
             return;
           }
-          
-          
+
           let listItemet = document.createElement("li");
           listItemet.innerHTML = item.title ? `${item.title}` : "no title ";
           item.checked
-          ? listItemet.classList.add("checkedItem")
-          : listItemet.classList.remove("checkedItem");
+            ? listItemet.classList.add("checkedItem")
+            : listItemet.classList.remove("checkedItem");
           listUl.appendChild(listItemet);
-          
+
           // previewObject.innerHTML += (item.title) ? `<li> ${item.title}  </li> ` : "no title ";
-          
+
           // previewObject.innerHTML += (item.title) ? `<li> ${item.title}  </li> ` : "no title ";
           // previewObject.innerHTML += (item.checked) ? `<span class="bold">checked:</span>${item.checked} ` : "";
-          
+
           count++;
         });
         // previewObject.innerHTML += + list.itemList.length - 5
@@ -117,18 +120,20 @@ export async function displayListsAlt() {
       let trashcan = document.createElement("span");
       trashcan.classList.add("remove-container", "hidden", "hover");
       trashcan.innerHTML =
-        '<svg class="remove hover" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
+        '<svg class="remove hover unclickable" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path class="unclickable" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
       // ---------- DELETE-FUNCTION ----------
       trashcan.addEventListener("click", (e) => {
         e.stopPropagation();
         let currentList = e.target.parentElement.parentElement;
-        console.log(`du klickar på ${currentList.getAttribute("data-list-id")}`);
+        console.log(
+          `du klickar på ${currentList.getAttribute("data-list-id")}`
+        );
         if (currentList.getAttribute("data-list-id") !== "null") {
           console.log(
             `du tog bort lista ${currentList.getAttribute("data-list-id")}`
-            );
-            deleteListUsingID(currentList.getAttribute("data-list-id"))
-          currentList.remove();
+          );
+          // deleteListUsingID(currentList.getAttribute("data-list-id"));
+          // currentList.remove();
         }
       });
       previewObject.appendChild(trashcan);
@@ -145,8 +150,6 @@ export async function displayListsAlt() {
   // const listItemsUl = document.querySelector("#listItems");
 
   let currentState = "viewOneList";
-
-  
 
   // updateItem(item._id,)
 

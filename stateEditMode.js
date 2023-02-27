cdbb22625551832105d47ff8e77479266f76db35
karlfredriko <kf.holmsten@gmail.com>
2023-02-27 14:26:41 +0100

    fix: :bug:

--- stateEditMode.js
+++ stateEditMode.js
@@ -80,7 +80,7 @@ export function editMode({ selectedList, listItemsUl, API_BASE, headerName }) {
       labelA.setAttribute("for", item._id);
       labelA.classList.add("itemContainer", "editing");
       labelA.innerHTML = `<span class="iconspans"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" id="${trashName}" width="14px"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></span>
-    <input type="text" value="${item.title}" id="item_${item._id}"></input>`;
+    <input type="text" onClick="this.setSelectionRange(0, this.value.length)" value="${item.title}" id="item_${item._id}"></input>`;
 
       item.checked
         ? listItem.classList.add("checkedItem")
@@ -142,7 +142,7 @@ export function editMode({ selectedList, listItemsUl, API_BASE, headerName }) {
     headerName.innerHTML = `
         <p class="thisIsEdit">Edit mode</p>
         <span class="backBtn hover"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></span>
-        <input type="text" class="nameinput list-color-header-${
+        <input type="text" onClick="this.setSelectionRange(0, this.value.length)" class="nameinput list-color-header-${
           selectedList.color ?? "default"
         }" value="${listNamn}" onfocus="this.placeholder=''"></input>
         <button id="button-editmode"><svg class="hover" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><use xlink:href="assets/three-dots-vertical.svg#three-dots-vertical"></use></button>
@@ -233,7 +233,7 @@ export function editMode({ selectedList, listItemsUl, API_BASE, headerName }) {
         } else {
           let li = document.createElement("li");
           li.innerHTML = `<span class="iconspans"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" id="${listItemInput.value}" width="14px"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></span>
-        <input type="text" value="${listItemInput.value}" id="item_${listItemInput.value}"></input>`;
+        <input type="text" onClick="this.setSelectionRange(0, this.value.length)" value="${listItemInput.value}" id="item_${listItemInput.value}"></input>`;
           listItemsUl.append(li);
           let removeBtn = document.getElementById(`${listItemInput.value}`);
           let inputfields = document.getElementById(
